a4b3ad4bdaefec65146eee8de88fbe0ab255d463
harsha <harsha.panja@xoriant.com>
2016-12-13 19:43:54 +0530

    change in file

--- src/app/crisis-list.component.ts
+++ src/app/crisis-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   template: `
-    <h2>CRISIS CENTER</h2>
+    <h2>CRISIS CENTER is here</h2>
     <p> {{date | date:'shortDate'}}Get your crisis here</p>`
 })
 export class CrisisListComponent {
